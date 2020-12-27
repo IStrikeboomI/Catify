dc3d786ad954f34fcaf6569eca963de99492db74
IStrikeboomI <shubhmakadia12@gmail.com>
2020-12-26 20:49:22 -0600

    Changed image link

--- scripts/convertImagesToCat.js
+++ scripts/convertImagesToCat.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(tab.ib, {
-       code: "for (x in document.getElementsByTagName('img')) {document.getElementsByTagName('img')[x].src = 'https://raw.githubusercontent.com/IStrikeboomI/cat/master/src/Strikeboom/cat/images/cat1.jpg'}"
+       code: "for (x in document.getElementsByTagName('img')) {document.getElementsByTagName('img')[x].src = 'https://raw.githubusercontent.com/IStrikeboomI/Catify/main/icons/full.jpg'}"
     });
 });
