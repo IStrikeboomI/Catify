c574b6fd47bf2af6a7aaaa9a99e0122b4ac91c29
Shubh Makadia <60586926+IStrikeboomI@users.noreply.github.com>
2021-11-21 14:01:22 -0600

    using a for of loop instead of for in

--- scripts/convertImagesToCat.js
+++ scripts/convertImagesToCat.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(tab.ib, {
-       code: "for (x in document.getElementsByTagName('img')) {document.getElementsByTagName('img')[x].src = 'https://raw.githubusercontent.com/IStrikeboomI/Catify/main/icons/full.jpg'}"
+       code: "for (let x of document.getElementsByTagName('img')) {x.src = 'https://raw.githubusercontent.com/IStrikeboomI/Catify/main/icons/full.jpg'}"
     });
 });
