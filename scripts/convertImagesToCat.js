3ea3ae1110fd063e7244390e4d67d6e255ee8ff3
Shubh Makadia <60586926+IStrikeboomI@users.noreply.github.com>
2021-11-21 14:16:03 -0600

    Added spacing between lines so it looks nicer

--- scripts/convertImagesToCat.js
+++ scripts/convertImagesToCat.js
@@ -1,5 +1,7 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(tab.ib, {
-       code: "for (let x of document.getElementsByTagName('img')) {x.src = 'https://raw.githubusercontent.com/IStrikeboomI/Catify/main/icons/full.jpg'}"
+       code: `for (let x of document.getElementsByTagName('img')) {
+		   x.src = 'https://raw.githubusercontent.com/IStrikeboomI/Catify/main/icons/full.jpg'
+		   }`
     });
 });
